00c9fa147c682d5bf5d8aa2b2b5541bb67199c86
jamesbakendalr115 <i.yalovaya.4651119@rambler.ru>
2025-12-04 10:10:58 +0800

    add hardhat.config.ts, utils-export-backend.jsx, advanced-secure-boilerplate.py

---
+++ advanced-secure-boilerplate.py
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\advanced-secure-boilerplate.py
+// Generated at: 2025-10-07T08:40:49.977Z
---
+++ frontend/web/src/utils-export-backend.jsx
@@ -0,0 +1,3 @@
+export const config = { runtime: 'edge' };
+
+export default function demo-subscriptions-profile() { return <div>Component</div>; }
